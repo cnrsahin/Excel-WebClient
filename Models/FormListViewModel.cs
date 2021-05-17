ba825ebf6e1252619b1248dd703c9ef9a2d0b42a
Caner SAHIN <caner.sahin@aryd.com.tr>
2021-05-17 10:52:52 +0300

    test

--- Models/FormListViewModel.cs
+++ Models/FormListViewModel.cs
@@ -9,5 +9,6 @@ namespace KoLeadForm.Models
     public class FormListViewModel
     {
         public List<FormDetail> FormDetails { get; set; }
+        public int MyProperty { get; set; }
     }
 }
